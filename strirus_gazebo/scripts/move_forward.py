ad243362a1f83d3779f9337ceaf191c583e836e4
Oleg Bulichev <obulichev@yandex.ru>
2017-06-08 10:15:43 +0300

    fix model physics

--- strirus_gazebo/scripts/move_forward.py
+++ strirus_gazebo/scripts/move_forward.py
@@ -6,7 +6,7 @@ pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 rospy.init_node('move_forward')
 r = rospy.Rate(10)
 msg = Twist()
-msg.linear.x = 0.7
+msg.linear.x = 0.4
 while not rospy.is_shutdown():
    pub.publish(msg)
    r.sleep()
